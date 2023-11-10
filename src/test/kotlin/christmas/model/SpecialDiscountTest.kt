244ea1fd9068494e929cf085568f345025b3e81f
somin <salth0601@gmail.com>
2023-11-11 01:28:53 +0900

    test: 특별 할인 기능 테스트 - isApplicable 메서드 테스트

---
+++ src/test/kotlin/christmas/model/SpecialDiscountTest.kt
@@ -0,0 +1,29 @@
+package christmas.model
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class SpecialDiscountTest {
+
+    @Nested
+    @DisplayName("isApplicable 메서드 테스트")
+    inner class IsApplicableTest {
+
+        @ParameterizedTest
+        @ValueSource(ints = [3, 10, 17, 24, 25, 31])
+        fun `달력에 별이 있는 날짜일 경우 true를 반환`(date: Int) {
+            val discount = SpecialDiscount(date)
+            assertTrue(discount.isApplicable())
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = [1, 8, 15, 22, 29])
+        fun `달력에 별이 있는 날짜가 아닐 경우 false를 반환`(date: Int) {
+            val discount = SpecialDiscount(date)
+            assertFalse(discount.isApplicable())
+        }
+    }
+}
